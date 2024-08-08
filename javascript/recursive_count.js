dfcff518165d55efa023dab55a7a58cb961de82c
Margarita Ramirez <margarita26.mr@gmail.com>
2024-08-08 17:21:23 -0400

    recursion

--- javascript/recursive_count.js
+++ javascript/recursive_count.js
@@ -1,6 +1,20 @@
+// recursive method:
 function recursiveCount(num = 0) {
-  // type your code here
+  if (num >= 10){
+    return;
+  }
+  console.log(num);
+  recursiveCount(++num);
+  console.log(num);
 }
+// while loop method:
+// function recursiveCount(num = 0) {
+//   let count = 0;
+//   while (count < 10){
+//     console.log(count);
+//     ++count;
+//   }
+// }
 
 if (require.main === module) {
   recursiveCount();
@@ -11,3 +25,18 @@ module.exports = recursiveCount;
 // OPTIONAL
 // Please add your pseudocode to this file
 // And a written explanation of your solution
+
+// THIS IS ALSO A VALID OPTION INSTEAD OF THE ABOVE
+// recursiveCount(++num);
+//  pre-increment: increments 'num' before the current expression is evaluated
+
+// THIS WILL CAUSE A STACK OVERFLOW. WHY?
+  // recursiveCount(num++);
+  // Post-increment: This increments 'num' after the current expression is evaluated
+  //  the value of 'num' before it is incremented is passed tot he next recursive call
+
+// IF YOU LOG NUM BELOW, THE NUMBERS PRINT BACKWARDS FROM 9 TO 0. WHY?
+// console.log(num);
+//  the logging occurs after the recursice call, so it only happens as the recuresion unwinds, 
+
+
